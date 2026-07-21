333701be1ce0d84732c3aa46b6298e324ed24dd9
Muhamad Jamaludin <muhhjam@gmail.com>
2026-07-21 23:05:50 +0700

    feat: replace local favicon with remote metadata icons in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,11 @@ const manrope = Manrope({
 export const metadata: Metadata = {
   title: "Skybook - Tebak Foto Masa Kecil",
   description: "Setiap gambar ada cerita. Tunjukan pesona kalian ke isi kantor Skyshi",
+  icons: {
+    icon: "https://lh3.googleusercontent.com/aida-public/AB6AXuA6WHca2xBvrLFfzPVz7j1xzPdu9IL2Kdb9sMyZgGbDud6M-hAAY62zasamdrdZ1rGwnkSozAmdsiaHwu-HMl52ruoSf4bQvwge8_LFWITHzmRC5gUH5eOdV3MBIQ5RsW-0BOfDMFT0WR6WmocJfrLjkNNxMMXWvnP2XQTGSXOinybCbtaa4qxQYxcMMzPpE_MTRCH5KjBrLCb4RdGERtFQiH63NzNEjJDcqzlCKk_ZQmU_yp3hM2FbO4FdY4tMWnz9yq1Txy_2T2Y",
+    shortcut: "https://lh3.googleusercontent.com/aida-public/AB6AXuA6WHca2xBvrLFfzPVz7j1xzPdu9IL2Kdb9sMyZgGbDud6M-hAAY62zasamdrdZ1rGwnkSozAmdsiaHwu-HMl52ruoSf4bQvwge8_LFWITHzmRC5gUH5eOdV3MBIQ5RsW-0BOfDMFT0WR6WmocJfrLjkNNxMMXWvnP2XQTGSXOinybCbtaa4qxQYxcMMzPpE_MTRCH5KjBrLCb4RdGERtFQiH63NzNEjJDcqzlCKk_ZQmU_yp3hM2FbO4FdY4tMWnz9yq1Txy_2T2Y",
+    apple: "https://lh3.googleusercontent.com/aida-public/AB6AXuA6WHca2xBvrLFfzPVz7j1xzPdu9IL2Kdb9sMyZgGbDud6M-hAAY62zasamdrdZ1rGwnkSozAmdsiaHwu-HMl52ruoSf4bQvwge8_LFWITHzmRC5gUH5eOdV3MBIQ5RsW-0BOfDMFT0WR6WmocJfrLjkNNxMMXWvnP2XQTGSXOinybCbtaa4qxQYxcMMzPpE_MTRCH5KjBrLCb4RdGERtFQiH63NzNEjJDcqzlCKk_ZQmU_yp3hM2FbO4FdY4tMWnz9yq1Txy_2T2Y",
+  }
 };
 
 export default function RootLayout({
